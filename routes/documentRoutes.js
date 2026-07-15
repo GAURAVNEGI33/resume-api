34588a9da6518fe79d769cde52138c8a223f8b40
Gaurav Negi <gauravnegigvps@gmail.com>
2026-07-15 21:02:26 +0530

    feat: add document routes

---
+++ routes/documentRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/documentController');
+
+// Test route
+router.get('/hello', controller.hello);
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Connect document routes
+router.use('/documents', require('./documentRoutes'));
+
+module.exports = router;
