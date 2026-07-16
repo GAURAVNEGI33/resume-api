89c020b4355132e7554f808fd1d156aeacbad865
Gaurav Negi <gauravnegigvps@gmail.com>
2026-07-16 20:15:07 +0530

    added some more function in documencontroller.js

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -22,3 +22,46 @@ function list(req, res) {
     });
   }
 }
+
+function create(req, res) {
+  res.send({
+    message: "Hello Everyone!",
+  });
+}
+
+/**
+ * Retrieve a document by id
+ * @param {*} req
+ * @param {*} res
+ */
+function findOne(req, res) {
+  try {
+    const id = req.params.id;
+    const document = documentModel.findById(id);
+
+    if (!document) {
+      return res.status(404).send({
+        success: false,
+        message: "Document not found.",
+      });
+    }
+
+    res.send({
+      success: true,
+      message: "Retrieved the document.",
+      document,
+    });
+  } catch (error) {
+    console.log("error in retrieve", error);
+    res.status(500).send({
+      success: false,
+      message: "Failed to retrieve the document.",
+    });
+  }
+}
+
+module.exports = {
+  list,
+  create,
+  findOne,
+};
