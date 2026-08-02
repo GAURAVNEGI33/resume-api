07ce71cc1f7c28cf620d313e3cd3d70b5648ad70
Gaurav Negi <gauravnegigvps@gmail.com>
2026-08-02 22:59:35 +0530

    chore: clean up backup folders and unused helper scripts

--- backup/routes/templates.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllTemplates, getTemplateById } = require('../controllers/templateController');
-
-router.get('/', getAllTemplates);
-router.get('/:id', getTemplateById);
-
-module.exports = router;
--- backup/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMe, updateMe, deleteMe } = require('../controllers/userController');
-
-router.get('/me', getMe);
-router.put('/me', updateMe);
-router.delete('/me', deleteMe);
-
-module.exports = router;
